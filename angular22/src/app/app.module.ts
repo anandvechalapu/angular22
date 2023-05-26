c100d1393f6cf523e783981d6f364fcdbc782796
dotnet-code <@dotnet-code>
2023-05-26 12:24:07 +0530

    commit from code202305262407

---
+++ angular22/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigurationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular22/src/app/configuration/configuration.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.css']
+})
+export class ConfigurationComponent implements OnInit {
+
+  githubUsername: string;
+  githubPassword: string;
+  githubURL: string;
+  githubRepositoryName: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    // submit logic
+  }
+
+}
